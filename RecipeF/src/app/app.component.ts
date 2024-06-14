e2d5fee28873766255bf2f362cc55d0e7996726a
S7M1R <sameeroddinkazi2002@gmail.com>
2024-06-14 23:56:18 +0530

    added component: navBar, Mian, Footer

--- RecipeF/src/app/app.component.ts
+++ RecipeF/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
