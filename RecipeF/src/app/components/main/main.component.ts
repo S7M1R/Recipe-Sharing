78f7828fc6c6e87461d16f16b5c9a353aef5359b
S7M1R <sameeroddinkazi2002@gmail.com>
2024-06-15 18:53:42 +0530

    Adding: RecipeCard, Fixed: main component

--- RecipeF/src/app/components/main/main.component.ts
+++ RecipeF/src/app/components/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- RecipeF/src/app/components/navbar/navbar.component.ts
+++ RecipeF/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenu } from '@angular/material/menu';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
-  templateUrl: './navbar.component.html',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenu],
+  templateUrl: './navbar.component.html', 
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  @ViewChild('menu') menu: MatMenu | undefined;
 }
